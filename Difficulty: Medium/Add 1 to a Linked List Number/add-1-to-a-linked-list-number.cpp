16cacc69dce9d67faa44cb74efab739e4b4a30f1
Abhishek Sharma <109891526+Abhishek-Sharma-1999@users.noreply.github.com>
2024-08-15 23:08:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Add 1 to a Linked List Number/add-1-to-a-linked-list-number.cpp
@@ -0,0 +1,122 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node* node) {
+    while (node != NULL) {
+        cout << node->data % 10;
+        node = node->next;
+    }
+    cout << "\n";
+}
+
+
+// } Driver Code Ends
+// User function template for C++
+
+/*
+
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+class Solution {
+    Node* reverse(Node* head){
+        Node* prev=NULL;
+        Node* curr=head;
+        Node* forward=NULL;
+        while(curr!=NULL)
+        {
+            forward=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=forward;
+        }
+        return prev;
+    }
+  public:
+    Node* addOne(Node* head) {
+        head=reverse(head);
+        int carry=1;
+        Node *temp=head;
+        Node* tail=NULL;
+        while(temp!=NULL && carry){
+            if(temp->data==9)
+            {
+                temp->data=0;
+            }
+            else{
+                temp->data=temp->data+1;
+                carry=0;
+            }
+            tail=temp;
+            temp=temp->next;
+        }
+        if(carry)
+        {
+            Node* newNode=new Node(carry);
+            tail->next=newNode;
+            newNode->next=NULL;
+        }
+        return reverse(head);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        if (arr.empty()) {
+            cout << -1 << endl;
+            continue;
+        }
+
+        int data = arr[0];
+        struct Node* head = new Node(data);
+        struct Node* tail = head;
+        for (int i = 1; i < arr.size(); ++i) {
+            data = arr[i];
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+        Solution ob;
+        head = ob.addOne(head);
+        printList(head);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
